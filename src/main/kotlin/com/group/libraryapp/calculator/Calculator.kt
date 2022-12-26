8755c86bea1fbf942ef6e65f8f4b8be770a2edd9
Hero <icraft2170@gmail.com>
2022-12-26 11:08:22 +0900

    코틀린의 Backing Field 사용하기

--- src/main/kotlin/com/group/libraryapp/calculator/Calculator.kt
+++ src/main/kotlin/com/group/libraryapp/calculator/Calculator.kt
@@ -3,23 +3,25 @@ package com.group.libraryapp.calculator
 import java.lang.IllegalArgumentException
 
 data class Calculator(
-    private var number: Int,
+    private var _number: Int,
 ) {
+    val number: Int
+        get() = this._number
 
     fun add(operand: Int) {
-        this.number += operand
+        this._number += operand
     }
 
     fun minus(operand: Int) {
-        this.number -= operand
+        this._number -= operand
     }
 
     fun multiply(operand: Int) {
-        this.number *= operand
+        this._number *= operand
     }
 
     fun division(operand: Int) {
         if (operand == 0) throw IllegalArgumentException("0으로 나눌 수 없습니다.")
-        this.number /= operand
+        this._number /= operand
     }
 }
