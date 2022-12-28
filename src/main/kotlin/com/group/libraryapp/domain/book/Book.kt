1e965a90928ab773055d95273aa775593cfbf3f9
Hero <icraft2170@gmail.com>
2022-12-29 00:00:08 +0900

    BookType 을 String -> Enum 으로 변경

--- src/main/kotlin/com/group/libraryapp/domain/book/Book.kt
+++ src/main/kotlin/com/group/libraryapp/domain/book/Book.kt
@@ -1,16 +1,14 @@
 package com.group.libraryapp.domain.book
 
 import java.lang.IllegalArgumentException
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
-import javax.persistence.Id
+import javax.persistence.*
 
 @Entity
 class Book (
     val name: String,
 
-    val type: String,
+    @Enumerated(EnumType.STRING)
+    val type: BookType,
 
     // 가장 아래 위치하는게 컨벤션
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -28,7 +26,7 @@ class Book (
     companion object {
         fun fixture(
             name: String = "책 이름",
-            type: String = "COMPUTER",
+            type: BookType = BookType.COMPUTER,
             id: Long? = null,
         ): Book {
             return Book(
