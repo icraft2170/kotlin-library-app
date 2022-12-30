22c1a1b89ec72098c11fc0e7a0de0ff66dc4d86c
Hero <icraft2170@gmail.com>
2022-12-30 23:39:13 +0900

    UserLoanHistory 조회 N+1 문제 해결(fetch조인) + 코드 리팩터링

--- src/main/kotlin/com/group/libraryapp/domain/user/UserRepository.kt
+++ src/main/kotlin/com/group/libraryapp/domain/user/UserRepository.kt
@@ -1,7 +1,11 @@
 package com.group.libraryapp.domain.user
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
 interface UserRepository : JpaRepository<User, Long> {
     fun findByName(name: String): User?
+
+    @Query("select DISTINCT u from User u LEFT JOIN FETCH UserLoanHistory uh on u = uh.user ")
+    fun findAllWithHistories(): List<User>
 }
--- src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
+++ src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
@@ -45,16 +45,7 @@ class UserService (
 
     @Transactional(readOnly = true)
     fun getUserLoanHistories(): List<UserLoanHistoryResponse> {
-        return userRepository.findAll().map { user ->
-            UserLoanHistoryResponse(
-                    name = user.name,
-                    books = user.userLoanHistories.map { userLoanHistory ->
-                        BookHistoryResponse(
-                            name = userLoanHistory.bookName,
-                            isReturn = userLoanHistory.status == UserLoanStatus.RETURNED
-                        )
-                    }
-                )
-            }
+        return userRepository.findAllWithHistories()
+            .map(UserLoanHistoryResponse::of)
     }
 }
